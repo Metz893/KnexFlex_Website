be2e492690a5a9dc7303ba8a0321cffad3b5bf68
Andrew Metz <andrewmetz@Mac.lan>
2025-12-14 16:20:26 -0800

    Fix Firebase SSR build error

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,19 +1,17 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyD5ntDf4dh1jxXXdkmPr1PD7AMm4aCHufk",
-  authDomain: "knexflex-4848a.firebaseapp.com",
-  projectId: "knexflex-4848a",
-  storageBucket: "knexflex-4848a.firebasestorage.app",
-  messagingSenderId: "47610877598",
-  appId: "1:47610877598:web:37ae6366ef11d503d18d8f",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
 
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
-// 🔑 what we actually need
 export const auth = getAuth(app);
 export const db = getFirestore(app);
