68d798dbf17c6ffc54b1e1d342cd887b011448f5
Andrew Metz <andrewmetz@Mac.lan>
2025-12-14 16:36:26 -0800

    Disable prerendering for online dashboard

--- app/online/dashboard/page.tsx
+++ app/online/dashboard/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useEffect, useState } from "react";
 import NavBar from "@/app/components/NavBar";
 import { collection, addDoc, serverTimestamp } from "firebase/firestore";
